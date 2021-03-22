c17c02bfd5b2c5c3211096edb8fc22c6dc802d2f
Mateus Dias <mateuussilvapb@gmail.com>
2021-03-22 13:15:49 -0300

    Alteração pequena almoço 22/03/2021

--- src/fundamentos/Aritmeticos.java
+++ src/fundamentos/Aritmeticos.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fundamentos;
 
-/**
- *
- * @author WELLIGHTON
- */
 public class Aritmeticos {
+    public static void main(String[] args) {
+        
+    }
     
 }
