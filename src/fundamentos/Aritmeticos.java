bb97c856a3b42cbf6789a1aeb442c4233061659b
Mateus Dias <mateuussilvapb@gmail.com>
2021-03-22 16:12:18 -0300

    Atlerações tarde de 22/03/2021

--- src/fundamentos/Aritmeticos.java
+++ src/fundamentos/Aritmeticos.java
@@ -1,8 +1,24 @@
 package fundamentos;
 
 public class Aritmeticos {
-    public static void main(String[] args) {
+    public static void aritmetico(String[] args) {
+        System.out.println(2 + 3);
+        double x = 34.56;
+        double y = 2.2;
         
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+        
+        int i = 8;
+        int j = 3;
+        System.out.println(i + j);
+        System.out.println(i - j);
+        System.out.println(i / j);
+        System.out.println((double)i / j);
+        System.out.println(i * j);
+        System.out.println(8 % 3);
     }
     
 }
