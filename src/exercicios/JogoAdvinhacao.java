c53be1674a93c6d4db0ecfcf0ad73b1e28c6703e
Mateus Dias <mateuussilvapb@gmail.com>
2021-03-29 10:54:37 -0300

    Alterações da manhã de 29/03/2021

--- src/exercicios/JogoAdvinhacao.java
+++ src/exercicios/JogoAdvinhacao.java
@@ -9,20 +9,42 @@ public class JogoAdvinhacao {
 
         int numAleatorio = (int) (Math.random() * 100) + 1;
         Scanner entrada = new Scanner(System.in);
-        
+
         int valor = 0;
         int tentativas = 1;
         System.out.println("JOGO DA ADVINHAÇÃO!");
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Informe um número: ");
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.print("Informe um número: ");
+//            valor = entrada.nextInt();
+//            if (valor == numAleatorio) {
+//                System.out.printf("Parbéns, você acertou! \nO número correto é: %d",
+//                        numAleatorio);
+//            } else if (valor > numAleatorio) {
+//                
+//            }
+//        }
+        do {
+            System.out.printf("Você está na dua %dº tentativa!\nInforme um número: ", tentativas);
             valor = entrada.nextInt();
-            if (valor == numAleatorio) {
-                System.out.printf("Parbéns, você acertou! \nO número correto é: %d",
-                        numAleatorio);
-            } else if (valor) {
-                
-            }
+            tentativas++;
+        } while (valor != numAleatorio && tentativas <= 10);
+        
+        if (valor == numAleatorio) {
+            System.out.printf("Parabén, você acertou!\n"
+                    + "O número gerado aleatoriamente foi: %d",
+                    numAleatorio);
+        } else if (tentativas == 11 && valor == numAleatorio) {
+            System.out.printf("Parabén, você acertou!\n"
+                    + "O número gerado aleatoriamente foi: %d",
+                    numAleatorio);
+        } else if (tentativas == 11 && valor != numAleatorio) {
+            System.out.printf("Infelizmente, não foi dessa vez!\n"
+                    + "O número aleatório foi: %d.\n", numAleatorio);
+        } else {
+            System.out.println("ERRO!");
         }
+
         entrada.close();
     }
 
