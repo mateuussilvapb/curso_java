82b38683e0e7a2125b24918e1971f64227eeea23
Mateus Dias <mateuussilvapb@gmail.com>
2021-05-10 10:50:30 -0300

    MÓDULO GENERICS AULAS INICIAIS E INTERMEDIÁRIAS

--- src/generics/ListaUtil.java
+++ src/generics/ListaUtil.java
@@ -14,5 +14,10 @@ public class ListaUtil {
     public static <T> T getUltimo2(List<T> lista) {
         return lista.get(lista.size() - 1);
     }
+    
+    public static <A, B, C> C teste(A paramA, B paramB){
+        C teste = null;
+        return teste;
+    }
 
 }
